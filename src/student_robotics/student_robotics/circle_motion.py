427030165dcf12443e592dbb567af7723670bdf6
MateoLopez00 <mathewlopez00@outlook.com>
2026-03-27 17:33:10 +0100

    [ADD] student_robotics & submission README

---
+++ src/student_robotics/student_robotics/circle_motion.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+import rclpy
+from rclpy.node import Node
+from geometry_msgs.msg import Twist
+
+class CircleMotion(Node):
+    def __init__(self):
+        super().__init__('circle_motion')
+        self.publisher = self.create_publisher(Twist, '/cmd_vel', 10)
+        self.timer = self.create_timer(0.1, self.publish_velocity)  # 10 Hz
+        self.get_logger().info('CircleMotion node started!')
+
+    def publish_velocity(self):
+        msg = Twist()
+        msg.linear.x = 0.3   # 0.3 m/s forward
+        msg.angular.z = 0.5  # 0.5 rad/s turn
+        self.publisher.publish(msg)
+        self.get_logger().info(f'Publishing: linear={msg.linear.x}, angular={msg.angular.z}')
+
+def main(args=None):
+    rclpy.init(args=args)
+    node = CircleMotion()
+    rclpy.spin(node)
+    rclpy.shutdown()
+
+if __name__ == '__main__':
+    main()
